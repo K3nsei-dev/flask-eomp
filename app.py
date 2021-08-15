edf0d4c6dc38ca4ef6356bd660c76915e93b8516
Jeandre Awood <jeandreross@gmail.com>
2021-08-15 15:29:19 +0200

    frontend fixes

--- app.py
+++ app.py
@@ -261,12 +261,12 @@ def add_products():
 
     # error trapping/handling
     try:
-        name = str(request.form['product_name'])
-        p_type = str(request.form['product_type'])
-        price = int(request.form['product_price'])
-        new_price = request.form['product_price']
-        description = str(request.form['product_description'])
-        image = str(request.form['product_image'])
+        name = str(request.json['product_name'])
+        p_type = str(request.json['product_type'])
+        price = int(request.json['product_price'])
+        new_price = request.json['product_price']
+        description = str(request.json['product_description'])
+        image = str(request.json['product_image'])
 
         if len(name) == 0 or len(p_type) == 0 or len(new_price) == 0 or len(description) == 0 or len(image) == 0:
             raise Exception("Please Fill In Each Section Correctly")
@@ -280,11 +280,11 @@ def add_products():
         raise TypeError("Please Use The Correct Values For Each Section")
 
     if request.method == "POST":  # inserting data into the database
-        product_name = request.form['product_name']
-        product_type = request.form['product_type']
-        product_price = request.form['product_price']
-        product_description = request.form['product_description']
-        product_image = request.form['product_image']
+        product_name = request.json['product_name']
+        product_type = request.json['product_type']
+        product_price = request.json['product_price']
+        product_description = request.json['product_description']
+        product_image = request.json['product_image']
 
         values = (product_name, product_type, product_price, product_description,
                   product_image)  # values of what must go in the database
